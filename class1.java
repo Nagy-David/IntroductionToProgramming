d12d541aeb70a4030abbc7111ccaadd0f3fcaff9
Nagy-David <47042907+Nagy-David@users.noreply.github.com>
2020-02-20 15:44:42 +0100

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,31 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    //A java program contains instructions(a command).
+	    //One of the most important instruction is the println.
+	    //It displays some information (text) on the standard output (screen).
+	    //The text to be displayed must be placed between quotes ("") signs.
+	   
+	    //We can create groups of instructions.
+	    //A group of instructions is called a block.
+	    //In Java a block is represented by {} symbols.
+	    //It is very important that each instruction is closed by a semicolon (;).
+		  System.out.println("Hello World");
+		  //Let's display our full name on the screen.
+		  System.out.println("Nagy Dávid\nDebrecen Hungary");
+		  // \n is the new line character
+
+     //Display a square filled with * symbols. The size of the square is 4.
+     System.out.println("****");
+     System.out.println("****");
+     System.out.println("****");
+     System.out.println("****");
+
+     //Display a empty square with * symbols. The size of the square is 4.
+	   System.out.println("****");
+     System.out.println("*  *");
+     System.out.println("*  *");
+     System.out.println("****");
+
+	}
+}
