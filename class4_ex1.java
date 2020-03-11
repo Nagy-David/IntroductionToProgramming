34877aa180f0de8b1638de42a0767cd35b668e6c
Nagy-David <47042907+Nagy-David@users.noreply.github.com>
2020-03-11 10:26:45 +0100

    Create class4_ex1.java

---
+++ class4_ex1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {	
+//Read a number (integer) from the standard input. If the number is divisible by 2 and 3, //then display "YES" and "NO" otherwise.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		if(n%2 == 0 && n%3 == 0){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}	
+	}
+}
